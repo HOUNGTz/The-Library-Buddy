ba0f761888be6d2e200ad5a8e0b7f395d36a7039
lyhoung <pov.lyhoung@e-power.com.kh>
2024-01-14 00:11:10 +0700

    create project structure

--- app/src/main/java/com/houng/mobile_app_development/modules/screens/HomeScreen.java
+++ app/src/main/java/com/houng/mobile_app_development/modules/screens/HomeScreen.java
@@ -11,6 +11,6 @@ public class HomeScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_page);
+        setContentView(R.layout.activity_home_screen);
     }
 }
--- app/src/main/java/com/houng/mobile_app_development/modules/screens/SignUpScreen.java
+++ app/src/main/java/com/houng/mobile_app_development/modules/screens/SignUpScreen.java
@@ -11,6 +11,6 @@ public class SignUpScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        setContentView(R.layout.activity_signup_screen);
     }
 }
