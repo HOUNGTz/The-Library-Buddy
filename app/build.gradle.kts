be15530f9786531f4f57c0e3f3ea22e59395e414
lyhoung <pov.lyhoung@e-power.com.kh>
2024-01-15 11:43:06 +0700

    fixed warning relate with responsive and and target sdk33.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     defaultConfig {
         applicationId = "com.houng.mobile_app_development"
         minSdk = 24
-        targetSdk = 33
+        //targetSdk = 33 //Policy center still shows a warning to target sdk 33
         versionCode = 1
         versionName = "1.0"
 
@@ -35,7 +35,7 @@ android {
 dependencies {
 
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
     testImplementation("junit:junit:4.13.2")
