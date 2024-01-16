d56a7a8b619b04915326a4c8d0fa033263cb63f1
Leang-Hor <134486379+Leang-Hor@users.noreply.github.com>
2024-01-17 00:17:01 +0700

    modify code

--- app/src/main/java/com/houng/mobile_app_development/modules/screens/LoginScreen.java
+++ app/src/main/java/com/houng/mobile_app_development/modules/screens/LoginScreen.java
@@ -1,14 +1,42 @@
 package com.houng.mobile_app_development.modules.screens;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.houng.mobile_app_development.MainButtomNavigation;
 import com.houng.mobile_app_development.R;
 
 public class LoginScreen extends AppCompatActivity {
-
+    public TextView btn_login;
+    public Button login;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+
+        btn_login = findViewById(R.id.sign_up_pag3);
+        login = findViewById(R.id.button_login);
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginScreen.this, SignUpScreen.class);
+                startActivity(intent);
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginScreen.this, MainButtomNavigation.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/houng/mobile_app_development/modules/screens/SignUpScreen.java
+++ app/src/main/java/com/houng/mobile_app_development/modules/screens/SignUpScreen.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import com.houng.mobile_app_development.MainButtomNavigation;
 import com.houng.mobile_app_development.R;
 
 public class SignUpScreen extends AppCompatActivity {
@@ -21,7 +20,7 @@ public class SignUpScreen extends AppCompatActivity {
         btn_register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(SignUpScreen.this, MainButtomNavigation.class);
+                Intent intent = new Intent(SignUpScreen.this, LoginScreen.class);
                 startActivity(intent);
             }
         });
