32ece35e8b246e32ab0f765ea301042f657097ed
Leang-Hor <134486379+Leang-Hor@users.noreply.github.com>
2024-01-20 17:26:22 +0700

    build dialog

--- app/src/main/java/com/houng/mobile_app_development/modules/pages/ProfilePage.java
+++ app/src/main/java/com/houng/mobile_app_development/modules/pages/ProfilePage.java
@@ -1,8 +1,12 @@
 package com.houng.mobile_app_development.modules.pages;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
-
 import android.app.Activity;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,10 +14,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
-
 import com.houng.mobile_app_development.R;
 
+import java.util.Objects;
+
 public class ProfilePage extends Fragment {
+    public LinearLayout evenLogout;
     @Override
     public View onCreateView(
         LayoutInflater inflater,
@@ -22,9 +28,17 @@ public class ProfilePage extends Fragment {
             View view = inflater.inflate(R.layout.activity_profile_page, container, false);
             setupClickListener(view, R.id.add_book, AddPage.class);
             setupClickListener(view, R.id.about, AboutUsPage.class);
-            return view;
+        evenLogout = view.findViewById(R.id.even_logout);
+        if (evenLogout != null) {
+            evenLogout.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    new StartGameDialogFragment().show(requireActivity().getSupportFragmentManager(), "GAME_DIALOG");
+                }
+            });
+        }
+        return view;
     }
-
     private void setupClickListener(View view, int layoutId, Class<?> activityClass) {
         LinearLayout layout = view.findViewById(layoutId);
         if (layout != null) {
@@ -42,4 +56,22 @@ public class ProfilePage extends Fragment {
 
         }
     }
+    public static class StartGameDialogFragment extends DialogFragment {
+        @NonNull
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(requireActivity());
+            builder.setIcon(R.mipmap.warning_bulelight_icon)
+                .setMessage(R.string.dialog_start_game)
+                .setPositiveButton(R.string.start, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {}
+                })
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {}
+                }
+            );
+            return builder.create();
+        }
+    }
 }
+
