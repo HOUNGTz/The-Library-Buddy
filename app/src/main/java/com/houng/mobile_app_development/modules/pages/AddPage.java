89eaad6e632fdbd74b37f469c8d805fe9b76385b
lyhoung <pov.lyhoung@e-power.com.kh>
2024-01-20 11:45:29 +0700

    more update

--- app/src/main/java/com/houng/mobile_app_development/modules/pages/AddPage.java
+++ app/src/main/java/com/houng/mobile_app_development/modules/pages/AddPage.java
@@ -1,9 +1,14 @@
 package com.houng.mobile_app_development.modules.pages;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.view.MenuItem;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
+
 import com.houng.mobile_app_development.R;
 
 public class AddPage extends AppCompatActivity {
@@ -13,11 +18,31 @@ public class AddPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_page);
 
-        ImageView arrow_back = findViewById(R.id.arrow_back);
-        arrow_back.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
+        //implement action bar.
+        Toolbar toolbar = findViewById(R.id.materialToolbar);
+        setSupportActionBar(toolbar);
+        // Set a custom title.
+        getSupportActionBar().setTitle("Insert New Books");
+        // Set the title text color (example: set to red)
+        int titleTextColor = ContextCompat.getColor(this, R.color.white); // Replace with your color resource
+
+        // Create a SpannableString with the desired color
+        SpannableString spannableString = new SpannableString(getSupportActionBar().getTitle());
+        spannableString.setSpan(new ForegroundColorSpan(titleTextColor), 0, spannableString.length(), 0);
+
+        // Set the SpannableString as the title
+        getSupportActionBar().setTitle(spannableString);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // Handle the navigation icon click
                 onBackPressed();
-            }
-        });
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
