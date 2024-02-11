c11fa06b6f44996bb60d37f1c1282a7bc7896404
lyhoung <pov.lyhoung@e-power.com.kh>
2024-02-11 21:45:26 +0700

    update ui about page

--- app/src/main/java/com/houng/mobile_app_development/modules/pages/AboutUsPage.java
+++ app/src/main/java/com/houng/mobile_app_development/modules/pages/AboutUsPage.java
@@ -1,26 +1,39 @@
 package com.houng.mobile_app_development.modules.pages;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
-
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.view.MenuItem;
 import com.houng.mobile_app_development.R;
+import java.util.Objects;
 
 public class AboutUsPage extends AppCompatActivity {
-
+    int titleTextColor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us_page);
 
-        ImageView iconTapBack = findViewById(R.id.about_arrow_back);
-        iconTapBack.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onBackPressed();
-            }
-        });
+        Toolbar toolbar = findViewById(R.id.aboutPage);
+        setSupportActionBar(toolbar);
+        Objects
+                .requireNonNull(getSupportActionBar())
+                .setTitle("About");
+        titleTextColor = ContextCompat.getColor(this, R.color.white);
+        SpannableString spannableString = new SpannableString(getSupportActionBar().getTitle());
+        spannableString.setSpan(new ForegroundColorSpan(titleTextColor), 0, spannableString.length(), 0);
+        getSupportActionBar().setTitle(spannableString);
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
